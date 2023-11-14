02a6cebbbb0487ff095a07bb8433f16466241d38
João Victor <ehojoaovictor@gmail.com>
2023-11-13 21:29:49 -0300

    up

--- Platformers/assets/scripts/player/Player.gd
+++ Platformers/assets/scripts/player/Player.gd
@@ -7,30 +7,36 @@ extends CharacterBody2D
 
 @onready var sprite = $Sprite
 
-@export var speed = 120
-@export var default_friction = 16
-var friction = default_friction
+@export var run_speed = 280
+@export var walk_speed = 140
+@onready var speed : float = run_speed
+@export var default_friction : float = 16
+var friction : float = default_friction
 
-@export var gravity = 420
-@export var jump_force = -180
+@export var gravity : float = 420
+@export var jump_force : float = -180
 
 var dash_effect = load("res://assets/instances/dash_effect.tscn")
 
 var direction = Vector2.ZERO
 
-var jumped = false
-var can_jump = true
-var can_dash = true
+var jumped : bool = false
+var can_jump : bool = true
+var can_dash : bool = true
 
-var dash_quantity = 1
+var dash_quantity : int = 1
 var dashs_left = dash_quantity
 
-var state = 0
+var state : int = 0
+
+const max_fall_speed : float = 500
+const max_ascend_speed : float = -500
 
 func _physics_process(delta):
 	match state:
 		0:
 			may_walk(delta)
+			may_wall_jump()
 			may_jump()
 			apply_gravity(delta)
 			
@@ -51,10 +57,19 @@ func get_input_direction():
 
 func apply_gravity(delta):
 	velocity.y += gravity * delta
+	
+	velocity.y = clamp(velocity.y, max_ascend_speed, max_fall_speed)
 
 
 func may_walk(delta):
 	var input = get_input_direction()
+	var extra_speed = max(0, velocity.length() - speed)
+	
+	print(
+		"Speed: %s, \nExtra speed: %s" %
+		
+		[ velocity.length(),
+		extra_speed])
 	
 	if input != Vector2.ZERO: 
 		direction = input.normalized()
@@ -63,12 +78,7 @@ func may_walk(delta):
 	if is_on_floor(): friction = lerpf(friction, default_friction, 8 * delta)
 	else:             friction = lerpf(friction, default_friction/4, 4 * delta)
 	
-	
-	if is_on_floor():
-		velocity.x = lerp(velocity.x, round(input.x) * speed, friction * delta)
-	else:
-		if input != Vector2.ZERO: velocity.x = lerp(velocity.x, round(input.x) * speed, friction * delta)
-		else: velocity.x = lerp(velocity.x, 0.0, friction/4 * delta)
+	velocity.x = lerp(velocity.x, round(input.x) * speed + + extra_speed, friction * delta)
 
 
 func may_jump():
@@ -84,11 +94,23 @@ func may_jump():
 		if not jump_buffer.is_stopped():
 			can_jump = false
 			velocity.y = jump_force
+			jump_buffer.stop()
 			if not Input.is_action_pressed("jump"): velocity.y /= 2
 	
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y /= 4
 
+
+func may_wall_jump():
+	if is_on_floor(): return
+	if not can_wall_jump(): return
+	
+	if not jump_buffer.is_stopped(): 
+		velocity.y = jump_force * 0.8
+		velocity.x = - direction.x * 480
+		
+		can_jump = false
+		jump_buffer.stop()
 
 func may_dash():
 	if not can_dash: return
@@ -96,26 +118,41 @@ func may_dash():
 	if is_on_floor():
 		dashs_left = dash_quantity
 	
-	if Input.is_action_just_pressed("special") and dashs_left > 0:
+	if dashs_left == 0: return
+	
+	if Input.is_action_just_pressed("downwards_dash"):
+		if is_on_floor(): return
+		direction = Vector2.DOWN + Vector2(round(direction.x), 0)
 		dashs_left -= 1
 		state = 1
+		can_jump = false
+		
 		dash_timer.start()
 		
 		add_child(dash_effect.instantiate())
+		
+	
+	if Input.is_action_just_pressed("frontwards_dash"):
+		if Input.is_action_pressed("up"): direction = Vector2(0, -0.6)
+		else: direction = Vector2(round(direction.x), -0.2)
+		
+		dashs_left -= 1
+		state = 1
+		can_jump = false
+		
+		dash_timer.start()
+		
+		add_child(dash_effect.instantiate())
+
+func can_wall_jump():
+	return ($RayCastWallLeft.is_colliding() or $RayCastWallRight.is_colliding()) and is_on_wall() 
 
 
 func _on_coyote_timer_timeout():
 	can_jump = false
 
-
 func _on_dash_timer_timeout():
 	state = 0
-	
-	velocity.y /= 4
-	if !is_on_floor(): 
-		velocity.x /= 4
-		friction = 0
-	
 	dash_cooldown_timer.start()
 	can_dash = false
 
